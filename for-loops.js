04b784a3baa5a19fd82421adb0e3b5af697d162f
Stephen Senft <stephensenft21@gmail.com>
2019-07-17 02:46:34 -0500

    final commit

---
+++ for-loops.js
@@ -0,0 +1,40 @@
+
+//  for (var Boom = 5; Boom <125; Boom += 5) {
+//      console.log(Boom)
+//      console.log("look at me")
+//   }
+
+//   for (var Bam = 4096; Bam > 1; Bam /= 2) {
+//       console.log(Bam)
+//         console.log(CAKE)
+//   }
+// let Presidents = ["George Washington","John Adams","Thomas Jefferson"]
+
+// for(let i = 0; i < Presidents.length; i++) {
+//     console.log(Presidents)
+//     alert("Sucka")
+// }
+
+
+var antSpecies = {
+    argentine: {},
+    army: {},
+    bigHeaded: {},
+    black: {},
+    bull: {},
+    carpenter: {},
+    crazy: {},
+    fire: {},
+    glider: {},
+    honeyPot: {},
+    jackJumper: {}
+  }
+ 
+
+  
+  let entries = Object.entries(antSpecies)
+  console.log(entries) 
+
+  for ( [antSpecies, antSpecies] of entries) {
+    console.log(`There are ${antSpecies} ${antSpecies}s`)
+  }
